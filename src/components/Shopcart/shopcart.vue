073d0ddf2742a921c7b92df0b6333a30ebec27d5
liuyishi <liuyishi@webmail.hzau.edu.cn>
2018-08-28 16:39:25 +0800

    add: add shoplist mask.

--- src/components/Shopcart/shopcart.vue
+++ src/components/Shopcart/shopcart.vue
@@ -1,57 +1,59 @@
 <template>
-  <div class="shopcart">
-    <div class="content" @click="toggleList">
-      <div class="content-left">
-        <div class="logo-wrapper">
-          <div class="logo" :class="{'highlight': totalCount>0}">
-            <i class="icon-shopping_cart" :class="{'highlight': totalCount>0}"></i>
+  <div>
+    <div class="shopcart">
+      <div class="content" @click="toggleList">
+        <div class="content-left">
+          <div class="logo-wrapper">
+            <div class="logo" :class="{'highlight': totalCount>0}">
+              <i class="icon-shopping_cart" :class="{'highlight': totalCount>0}"></i>
+            </div>
+            <div class="num" v-if="totalCount>0">{{totalCount}}</div>
           </div>
-          <div class="num" v-if="totalCount>0">{{totalCount}}</div>
+          <div class="price">¥{{totalPrice}}元</div>
+          <div class="desc">另需配送费¥{{deliveryPrice}}元</div>
         </div>
-        <div class="price">¥{{totalPrice}}元</div>
-        <div class="desc">另需配送费¥{{deliveryPrice}}元</div>
-      </div>
-      <div class="content-right">
-        <div class="pay" :class="payClass">
-          {{payDesc}}
-        </div>
-      </div>
-    </div>
-    <div class="ball-container">
-      <div v-for="ball in balls">
-        <transition name="drop" @before-enter="beforeDrop" @enter="dropping" @after-enter="afterDrop">
-          <div class="ball" v-show="ball.show">
-            <div class="inner inner-hook"></div>
+        <div class="content-right" @click.stop.prevent="pay">
+          <div class="pay" :class="payClass">
+            {{payDesc}}
           </div>
-
-        </transition>
+        </div>
       </div>
+      <div class="ball-container">
+        <div v-for="ball in balls">
+          <transition name="drop" @before-enter="beforeDrop" @enter="dropping" @after-enter="afterDrop">
+            <div class="ball" v-show="ball.show">
+              <div class="inner inner-hook"></div>
+            </div>
+          </transition>
+        </div>
+      </div>
+      <transition name="fold">
+        <div class="shopcart-list" v-show="listShow">
+          <div class="list-header">
+            <h1 class="title">购物车</h1>
+            <span class="empty" @click="empty">清空</span>
+          </div>
+          <div class="list-content" ref="listContent">
+            <ul>
+              <li class="food" v-for="(food,index) in selectFoods" :key="index">
+                <span class="name">{{food.name}}</span>
+                <div class="price">
+                  <span>¥{{food.price * food.count}}</span>
+                </div>
+                <div class="cartcontrol-wrapper">
+                  <v-cart-control @add="addFood" :food="food"></v-cart-control>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </transition>
+
+
     </div>
-    <transition name="fold">
-      <div class="shopcart-list" v-show="listShow">
-        <div class="list-header">
-          <h1 class="title">购物车</h1>
-          <span class="empty" @click="empty">清空</span>
-        </div>
-        <div class="list-content" ref="listContent">
-          <ul>
-            <li class="food" v-for="(food,index) in selectFoods" :key="index">
-              <span class="name">{{food.name}}</span>
-              <div class="price">
-                <span>¥{{food.price * food.count}}</span>
-              </div>
-              <div class="cartcontrol-wrapper">
-                <v-cart-control @add="addFood" :food="food"></v-cart-control>
-              </div>
-            </li>
-          </ul>
-        </div>
-
-
-      </div>
+    <transition name="fade">
+      <div class="list-mask" @click="hideList" v-show="listShow"></div>
     </transition>
-
-
   </div>
 </template>
 
@@ -145,6 +147,14 @@
       },
     },
     methods: {
+
+      pay() {
+        if (this.totalPrice >= this.minPrice) {
+          window.alert(`支付¥${this.totalPrice + this.deliveryPrice}元`
+          )
+        }
+      },
+
 
       toggleList() {
         if (!this.totalCount) {
@@ -353,7 +363,7 @@
       width: 100%;
       transform: translate3d(0, -100%, 0);
       &.fold-enter-active, &.fold-leave-active {
-        transition: all 0.4s linear;
+        transition: all 0.4s;
       }
       &.fold-enter, &.fold-leave-to {
         transform: translate3d(0, 0, 0);
@@ -407,6 +417,25 @@
 
         }
       }
+    }
+  }
+
+  .list-mask {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 40;
+    backdrop-filter: blur(10px);
+    opacity: 1;
+    background: rgba(7, 17, 27, 0.6);
+    &.fade-enter-active, &.fade-leave-active {
+      transition: all 0.4s;
+    }
+    &.fade-enter, &.fade-leave-to {
+      opacity: 0;
+      background: rgba(7, 17, 27, 0);
     }
 
   }
